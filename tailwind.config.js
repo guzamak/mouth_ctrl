110f623716f826a610b39e3ad332fd393fbdb7ab
guzamak <noipoke164@gmail.com>
2023-10-27 15:18:11 +0700

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,8 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        primary:'#F2F210',
-        secondary:{
-          '100':'#030303',
-          '200':'#F0F0F0',
-          '300':'#000000',
-          '400':'#000000',
-        }
-      },
       fontFamily:{
         'KAUFMANN':['KAUFMANN',"sans-serif"],
-        'NeutraText-Demi':['NeutraText-Demi',"sans"],
         'IBMPlexSansThai':['IBMPlexSansThai',"sans"],
       }
     },
